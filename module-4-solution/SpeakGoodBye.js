d2443cf812d6a23e318c80051d09b8068e4ef645
codeweb98 <64117819+codeweb98@users.noreply.github.com>
2020-05-30 12:25:07 +0530

    Update SpeakGoodBye.js

--- module-4-solution/SpeakGoodBye.js
+++ module-4-solution/SpeakGoodBye.js
@@ -2,7 +2,7 @@
 (function(window) {
   var byeSpeaker = {};
   var speakWord = "Good Bye";
-  byeSpeaker.speak = function(name) {
+  byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
   }
 window.byeSpeaker = byeSpeaker;
